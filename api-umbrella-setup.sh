182f1736b38067ee019e7ae6816fb6984b9d3617
Manjunath Somashekar <ujnamss@gmail.com>
2018-10-02 00:31:43 -0700

    granted +x for shell scripts

--- api-umbrella-setup.sh
+++ api-umbrella-setup.sh
@@ -1,4 +1,4 @@
 # Create a directory called zb for zerobugz
 mkdir -p zb/api-umbrella/config
 cp api-umbrella.yml zb/api-umbrella/config/api-umbrella.yml
-docker run -d --name=api-umbrella -p 8080:8080 -p 8443:8443 -v "$(pwd)/zb/api-umbrella/config":/etc/api-umbrella nrel/api-umbrella
+docker run -d --name=api-umbrella -net=host -p 8080:8080 -p 8443:8443 -v "$(pwd)/zb/api-umbrella/config":/etc/api-umbrella nrel/api-umbrella
