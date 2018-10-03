28ba6b3222fd947e86266dc0d2f1a8ae654fc85b
Manjunath Somashekar <ujnamss@gmail.com>
2018-10-02 17:42:04 -0700

    trying to expose port 443 and see if that works

--- api-umbrella-setup.sh
+++ api-umbrella-setup.sh
@@ -1,4 +1,5 @@
 # Create a directory called zb for zerobugz
 mkdir -p zb/api-umbrella/config
 cp api-umbrella.yml zb/api-umbrella/config/api-umbrella.yml
-docker run --name=api-umbrella -p 8080:8080 -p 8443:8443 -v "$(pwd)/zb/api-umbrella/config":/etc/api-umbrella nrel/api-umbrella
+# docker run --name=api-umbrella -p 8080:8080 -p 8443:8443 -v "$(pwd)/zb/api-umbrella/config":/etc/api-umbrella nrel/api-umbrella
+docker run --name=api-umbrella -p 443:443 -v "$(pwd)/zb/api-umbrella/config":/etc/api-umbrella nrel/api-umbrella
