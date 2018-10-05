748b5fa15e88d3946f1f25c5a24eaf7f7b70df57
Manjunath Somashekar <ujnamss@gmail.com>
2018-10-05 15:37:34 -0700

    generate cert only

--- zb_certs_lets_encrypt.sh
+++ zb_certs_lets_encrypt.sh
@@ -2,5 +2,6 @@ sudo apt-get update
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get update
-sudo apt-get install python-certbot-nginx
-sudo certbot --nginx certonly
+# sudo apt-get install python-certbot-nginx
+sudo apt-get install certbot
+sudo certbot certonly
